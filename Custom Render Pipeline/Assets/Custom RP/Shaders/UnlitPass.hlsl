71fc3b9db1f73467085654b1e44bc0ecc1135a10
Nicholas Skupien <nicholasskupien@gmail.com>
2021-08-11 18:36:08 -0400

    switch to unity core render pipeline functions

--- Custom Render Pipeline/Assets/Custom RP/Shaders/UnlitPass.hlsl
+++ Custom Render Pipeline/Assets/Custom RP/Shaders/UnlitPass.hlsl
@@ -2,13 +2,15 @@
 #define CUSTOM_UNLIT_PASS_INCLUDED
 #include "../ShaderLibrary/Common.hlsl"
 
+float4 _BaseColor;
+
 float4 UnlitPassVertex (float3 positionOS : POSITION) : SV_POSITION {
 	float3 positionWS = TransformObjectToWorld(positionOS.xyz);
 	return TransformWorldToHClip(positionWS);
 }
 
 float4 UnlitPassFragment () : SV_TARGET {
-	return 0.0;
+	return _BaseColor;
 }
 
 #endif
